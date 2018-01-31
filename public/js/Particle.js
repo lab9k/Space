817616de58ffcb9e42e1abe7ba5f92de9bb71e7d
Jorg Wieme <jorg.wieme@ugent.be>
2018-01-31 13:39:22 +0100

    replaced some code

--- public/js/Particle.js
+++ public/js/Particle.js
@@ -27,25 +27,25 @@ Particle.prototype = (function(o) {
 
         this._latest.set(this);
         this.add(this._speed);
+    },
+
+    render: function(ctx) {
+        if (this._speed.length() > 12) this._speed.normalize().scale(12);
+
+        this._latest.set(this);
+        this.add(this._speed);
+
+        ctx.save();
+        ctx.fillStyle = ctx.strokeStyle = '#fff';
+        ctx.lineCap = ctx.lineJoin = 'round';
+        ctx.lineWidth = this.radius * 2;
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this._latest.x, this._latest.y);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fill();
+        ctx.restore();
     }
-
-    // render: function(ctx) {
-    //     if (this._speed.length() > 12) this._speed.normalize().scale(12);
-
-    //     this._latest.set(this);
-    //     this.add(this._speed);
-
-    //     ctx.save();
-    //     ctx.fillStyle = ctx.strokeStyle = '#fff';
-    //     ctx.lineCap = ctx.lineJoin = 'round';
-    //     ctx.lineWidth = this.radius * 2;
-    //     ctx.beginPath();
-    //     ctx.moveTo(this.x, this.y);
-    //     ctx.lineTo(this._latest.x, this._latest.y);
-    //     ctx.stroke();
-    //     ctx.beginPath();
-    //     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    //     ctx.fill();
-    //     ctx.restore();
-    // }
 });
